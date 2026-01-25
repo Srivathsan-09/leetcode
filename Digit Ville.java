92ea3a37a4542468ca87b76398450885022a51b6
Srivathsan N <srivathsan0906@gmail.com>
2026-01-25 23:30:53 +0530

    Commit 9

---
+++ Digit Ville.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        int[] ans = new int[2];
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                ans[index++] = nums[i];
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+}
